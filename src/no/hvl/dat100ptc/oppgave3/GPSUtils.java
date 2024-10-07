80291821121251ebd5e779bcdb164d1c57ce3d06
t184914 <tor.tsn@gmail.com>
2024-10-07 20:22:20 +0200

    Begynt på oppgave 3
    Får og et par feil på distanserekninga, veit ikkje heilt ka feilen er

--- src/no/hvl/dat100ptc/oppgave3/GPSUtils.java
+++ src/no/hvl/dat100ptc/oppgave3/GPSUtils.java
@@ -25,26 +25,38 @@ public class GPSUtils {
 	public static double findMin(double[] da) {
 
 		double min;
-
-		// TODO 
-		throw new UnsupportedOperationException(TODO.method());
 		
+		min = da[0];
+		
+		for (double d : da) {
+			if (d < min) {
+				min = d;
+			}
+		}
+		
+		return min;
+
 	}
 
 	public static double[] getLatitudes(GPSPoint[] gpspoints) {
 
-		throw new UnsupportedOperationException(TODO.method());
+		double[] latitudes = new double[gpspoints.length];
 		
-		// TODO
+		for (int i = 0; i < gpspoints.length; i++) {
+			latitudes[i] = gpspoints[i].getLatitude();
+		}
+		return latitudes;
 	}
 
 	public static double[] getLongitudes(GPSPoint[] gpspoints) {
 
 		
-		throw new UnsupportedOperationException(TODO.method());
+		double[] longitudes = new double[gpspoints.length];
 		
-		// TODO 
-
+		for (int i = 0; i < gpspoints.length; i++ ) {
+			longitudes[i] = gpspoints[i].getLongitude();
+		}
+		return longitudes;
 	}
 
 	private static final int R = 6371000; // jordens radius
@@ -53,27 +65,33 @@ public class GPSUtils {
 
 		double d;
 		double latitude1, longitude1, latitude2, longitude2;
+		
+		latitude1 = Math.toRadians(gpspoint1.getLatitude());
+		longitude1 = Math.toRadians(gpspoint1.getLongitude());
+		latitude2 = Math.toRadians(gpspoint2.getLatitude());
+		longitude2 = Math.toRadians(gpspoint2.getLongitude());
+		
+		double deltaPhi = latitude2 - latitude1;
+		double deltadelta = longitude2 - longitude1;
 
-		throw new UnsupportedOperationException(TODO.method());
-
-		// TODO 
+		
+		double a = compute_a(latitude1, latitude2, deltaPhi, deltadelta);
+		double c = compute_c(a);
+		
+		return R * c;
 	}
 	
 	private static double compute_a(double phi1, double phi2, double deltaphi, double deltadelta) {
 	
-		throw new UnsupportedOperationException(TODO.method());
-		
-		// TODO 
+		return Math.sin(deltaphi / 2) * Math.sin(deltaphi / 2) + Math.cos(phi1) * Math.cos(phi2) * 
+				Math.cos(deltadelta / 2) * Math.sin(deltadelta / 2);
 
 	}
 
 	private static double compute_c(double a) {
 
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
-		
-		// TODO 
+		return 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));					
+		 
 
 	}
 
